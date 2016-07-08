2c61ba5df382d1755d48dc21b1ddd373aaf92168
Michał Siwek <mike21@aol.pl>
2016-07-08 12:22:06 +0200

    Allow setting tintColor

--- RCTFSafariViewController.m
+++ RCTFSafariViewController.m
@@ -4,7 +4,7 @@
 
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(openURL:(NSString *)urlString) {
+RCT_EXPORT_METHOD(openURL:(NSString *)urlString params:(NSDictionary *)params) {
   NSURL *url = [[NSURL alloc] initWithString:urlString];
   
   UIViewController *rootViewController = [[UIApplication sharedApplication] keyWindow].rootViewController;
@@ -13,6 +13,11 @@ RCT_EXPORT_METHOD(openURL:(NSString *)urlString) {
   
   [navigationController setNavigationBarHidden:YES animated:NO];
   safariViewController.delegate = self;
+  
+  if ([params objectForKey:@"tintColor"]) {
+    UIColor *tintColor = [RCTConvert UIColor:params[@"tintColor"]];
+    safariViewController.view.tintColor = tintColor;
+  }
   
   dispatch_async(dispatch_get_main_queue(), ^{
     [rootViewController presentViewController:navigationController animated:YES completion:nil];

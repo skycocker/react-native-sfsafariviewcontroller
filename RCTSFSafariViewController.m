db93855aedfea4fa5539320522dd4aa08f6f1bfc
Michał Siwek <mike21@aol.pl>
2016-09-28 17:10:41 +0200

    Handle older and latest iOS versions

--- RCTSFSafariViewController.m
+++ RCTSFSafariViewController.m
@@ -12,19 +12,24 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(openURL:(NSString *)urlString params:(NSDictionary *)params) {
   NSURL *url = [[NSURL alloc] initWithString:urlString];
-  
-  UIViewController *rootViewController = [[UIApplication sharedApplication] keyWindow].rootViewController;
+
+  UIViewController *rootViewController = [[[UIApplication sharedApplication] delegate] window].rootViewController;
   SFSafariViewController *safariViewController = [[SFSafariViewController alloc] initWithURL:url];
   UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:safariViewController];
-  
+
   [navigationController setNavigationBarHidden:YES animated:NO];
   safariViewController.delegate = self;
-  
+
   if ([params objectForKey:@"tintColor"]) {
     UIColor *tintColor = [RCTConvert UIColor:params[@"tintColor"]];
-    safariViewController.preferredControlTintColor = tintColor;
+
+    if([safariViewController respondsToSelector:@selector(setPreferredControlTintColor:)]) {
+      safariViewController.preferredControlTintColor = tintColor;
+    } else {
+      safariViewController.view.tintColor = tintColor;
+    }
   }
-  
+
   dispatch_async(dispatch_get_main_queue(), ^{
     [rootViewController presentViewController:navigationController animated:YES completion:^{
       [self.bridge.eventDispatcher sendDeviceEventWithName:@"SFSafariViewControllerDidLoad" body:nil];
